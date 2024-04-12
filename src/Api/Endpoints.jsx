f5c8c101442116870481180364dc1845c5f392f2
khansk13 <137790753+khansk13@users.noreply.github.com>
2024-04-12 11:53:46 +0530

    commit

--- src/Api/Endpoints.jsx
+++ src/Api/Endpoints.jsx
@@ -1,10 +1,10 @@
 // authentication endpoints
-export const signupEndpoints = '/auth/v1/signup'
-export const signinEndpoints = '/auth/v1/signin'
+export const signupEndpoints = "/auth/signup";
+export const signinEndpoints = "/auth/signin";
 
 // employee endpoints
-export const addEmployeeEndpoint = '/employees/v1/add-employee';
-export const viewMyEmployeeEndpoint = '/employees/v1/view-employee';
-export const viewIndiEmployeeEndpoint = '/employees/v1/view-indi-employee';
-export const updateEmployeeEndpoint = '/employees/v1/edit-employee';
-export const deleteEmployeeEndpoint = '/employees/v1/delete-employee';
+export const addEmployeeEndpoint = "/employees";
+export const viewMyEmployeeEndpoint = "/employees";
+export const viewIndiEmployeeEndpoint = "/employees";
+export const updateEmployeeEndpoint = "/employees";
+export const deleteEmployeeEndpoint="/employees";
